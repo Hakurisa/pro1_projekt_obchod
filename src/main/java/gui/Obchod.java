710f9a3effd2d5e27417b6773f87e2aa118590c5
Hakurisa <bottleofscrumpy@gmail.com>
2023-04-30 17:05:37 +0200

    ask for save on exit

--- src/main/java/gui/Obchod.java
+++ src/main/java/gui/Obchod.java
@@ -3,6 +3,8 @@ package gui;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import lib.Kosik;
 import lib.Sklad;
@@ -22,7 +24,6 @@ public class Obchod {
     private JPanel panelKosiku;
     private JTable tabulkaKosiku;
 
-
     private Sklad sklad;
     private Kosik kosik;
     private JLabel lbCena;
@@ -35,9 +36,14 @@ public class Obchod {
         vytvorKomponenty();
     }
 
+    public Sklad getSklad() {
+        return this.sklad;
+    }
+
     public JPanel getHlavniPanel() {
         return this.hlavniPanel;
     }
+
     public JMenuBar getNabidka() {
         return this.nabidka;
     }
@@ -46,8 +52,8 @@ public class Obchod {
         vytvorNabidku();
         vytvorPanelSkladu();
         vytvorPanelKosiku();
-        
-        hlavniPanel = new JPanel(); 
+
+        hlavniPanel = new JPanel();
         JTabbedPane tabbedPane = new JTabbedPane();
         hlavniPanel.add(tabbedPane);
         tabbedPane.addTab("Sklad", panelSkladu);
@@ -73,17 +79,17 @@ public class Obchod {
         pnTlacitka.add(btKosikPridej);
         pnTlacitka.add(btSmaz);
         pnTlacitka.add(btSmazVse);
-        
+
         btSmaz.setEnabled(false);
         btKosikPridej.setEnabled(false);
 
-        
-        //checks if a row is selected and if yes, enables the delete button
-        //english comment because coding in czech physically hurts me sorry
+        // checks if a row is selected and if yes, enables the delete button
+        // english comment because coding in czech physically hurts me sorry
         tabulkaSkladu.getSelectionModel().addListSelectionListener((e) -> {
             int radek = tabulkaSkladu.getSelectedRow();
-            //apparently when nothing is selected it reports as -1 so this works don't question it
-            if(radek < 0) {
+            // apparently when nothing is selected it reports as -1 so this works don't
+            // question it
+            if (radek < 0) {
                 btSmaz.setEnabled(false);
                 btKosikPridej.setEnabled(false);
             } else {
@@ -92,17 +98,17 @@ public class Obchod {
             }
         });
 
-        
-        btPridej.addActionListener((e)-> {
+        btPridej.addActionListener((e) -> {
             sklad.pridejZbozi(new Zbozi("-", 0, 0));
         });
 
         btKosikPridej.addActionListener((e) -> {
             int radek = tabulkaSkladu.getSelectedRow();
             Zbozi zbozi = sklad.getZbozi(radek);
-            //TODO: make this better
-            if(zbozi.getPocet() <= 0) {
-                JOptionPane.showMessageDialog(null, "Na skladě již žádný produkt " + zbozi.getNazev() + " není!", "Varování", JOptionPane.PLAIN_MESSAGE);
+            // TODO: make this better
+            if (zbozi.getPocet() <= 0) {
+                JOptionPane.showMessageDialog(null, "Na skladě již žádný produkt " + zbozi.getNazev() + " není!",
+                        "Varování", JOptionPane.PLAIN_MESSAGE);
             } else {
                 kosik.pridej(zbozi);
                 lbCena.setText(kosik.cenaCelkem());
@@ -120,8 +126,10 @@ public class Obchod {
         });
 
         btSmazVse.addActionListener((e) -> {
-            int confrimDialog = JOptionPane.showConfirmDialog(null, "Smažete všechny položky v tabulce!\nOpravdu chcete pokračovat?", "Varování!" ,JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
-            if(confrimDialog == JOptionPane.YES_OPTION) {
+            int confrimDialog = JOptionPane.showConfirmDialog(null,
+                    "Smažete všechny položky v tabulce!\nOpravdu chcete pokračovat?", "Varování!",
+                    JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+            if (confrimDialog == JOptionPane.YES_OPTION) {
                 sklad.smazatVsechnoZbozi();
                 kosik.smazatVsechnoZbozi();
                 lbCena.setText(kosik.cenaCelkem());
@@ -140,7 +148,7 @@ public class Obchod {
         JMenu menuNapoveda = new JMenu();
         JMenuItem miUkoncit = new JMenuItem();
         JMenuItem miAbout = new JMenuItem("O programu");
-        
+
         menuSoubor.setText("Soubor");
         miUkoncit.setText("Ukončit");
         menuSoubor.add(miUkoncit);
@@ -149,10 +157,36 @@ public class Obchod {
         miUkoncit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
-                System.exit(0);
+                boolean saved = false;
+                int confirmed = JOptionPane.showConfirmDialog(null,
+                        "Chcete uložit změny?", "Pozor",
+                        JOptionPane.YES_NO_CANCEL_OPTION);
+                if (confirmed == JOptionPane.YES_OPTION) {
+                    try {
+                        JFileChooser dialog = new JFileChooser(".");
+                        dialog.setFileFilter(new FileNameExtensionFilter("*.json", "json"));
+                        if (dialog.showSaveDialog(dialog) == JFileChooser.APPROVE_OPTION) {
+                            String soubor = dialog.getSelectedFile().getPath();
+                            sklad.uloz(new GsonSerDes(), soubor + ".json");
+                            saved = true;
+                        }
+                    } catch (Exception exp) {
+                        JOptionPane.showMessageDialog(menuNapoveda,
+                                "Při ukládání do JSON formátu nastala: "
+                                        + exp.getLocalizedMessage(),
+                                "Chyba ukládání",
+                                JOptionPane.ERROR_MESSAGE);
+                    }
+                    if (saved) {
+                        System.exit(0);
+                    }
+                } else if (confirmed == JOptionPane.NO_OPTION) {
+                    System.exit(0);
+                }
             }
         });
-        miAbout.addActionListener((e) -> JOptionPane.showMessageDialog(hlavniPanel, "uwoogh", "Herní košík omg Java momentka", JOptionPane.INFORMATION_MESSAGE));
+        miAbout.addActionListener((e) -> JOptionPane.showMessageDialog(hlavniPanel, "uwoogh",
+                "Herní košík omg Java momentka", JOptionPane.INFORMATION_MESSAGE));
         JMenuItem miNactiJson = new JMenuItem("Načti JSON");
         miNactiJson.addActionListener((e) -> {
             try {
@@ -165,7 +199,8 @@ public class Obchod {
             } catch (Exception exp) {
                 JOptionPane.showMessageDialog(hlavniPanel,
                         "Při načítání do JSON formátu nastala: "
-                                + exp.getLocalizedMessage(), "Chyba načítání",
+                                + exp.getLocalizedMessage(),
+                        "Chyba načítání",
                         JOptionPane.ERROR_MESSAGE);
             }
         });
@@ -182,12 +217,13 @@ public class Obchod {
             } catch (Exception exp) {
                 JOptionPane.showMessageDialog(hlavniPanel,
                         "Při ukládání do JSON formátu nastala: "
-                            + exp.getLocalizedMessage(), "Chyba ukládání",
+                                + exp.getLocalizedMessage(),
+                        "Chyba ukládání",
                         JOptionPane.ERROR_MESSAGE);
             }
         });
         menuSoubor.add(miNactiJson);
-        menuSoubor.add(miUlozJson);        
+        menuSoubor.add(miUlozJson);
         nabidka = new JMenuBar();
         nabidka.add(menuSoubor);
         nabidka.add(menuNapoveda);
@@ -200,7 +236,7 @@ public class Obchod {
         tabulkaKosiku.setFillsViewportHeight(true);
         JScrollPane spTabulkaKosiku = new JScrollPane(tabulkaKosiku);
 
-        //cart GUI
+        // cart GUI
 
         JButton btPridej = new JButton("Zvýšit počet");
         JButton btOdeberJeden = new JButton("Snížit počet");
@@ -213,8 +249,9 @@ public class Obchod {
 
         tabulkaKosiku.getSelectionModel().addListSelectionListener((e) -> {
             int radek = tabulkaKosiku.getSelectedRow();
-            //apparently when nothing is selected it reports as -1 so this works don't question it
-            if(radek < 0) {
+            // apparently when nothing is selected it reports as -1 so this works don't
+            // question it
+            if (radek < 0) {
                 btPridej.setEnabled(false);
                 btOdeberJeden.setEnabled(false);
                 btSmaz.setEnabled(false);
@@ -224,29 +261,30 @@ public class Obchod {
                 btSmaz.setEnabled(true);
             }
         });
-        //button action listeners
-        btPridej.addActionListener((e)-> {
+        // button action listeners
+        btPridej.addActionListener((e) -> {
             int radek = tabulkaKosiku.getSelectedRow();
             Zbozi zbozi = (Zbozi) tabulkaKosiku.getValueAt(radek, 0);
-            if(!sklad.checkIfZero(zbozi)) {
+            if (!sklad.checkIfZero(zbozi)) {
                 sklad.odeberJednoZbozi(zbozi);
                 kosik.zvysit(radek);
             } else {
-                JOptionPane.showMessageDialog(hlavniPanel, "Požadované zboží již není na skladě!", "Varování", JOptionPane.WARNING_MESSAGE);
+                JOptionPane.showMessageDialog(hlavniPanel, "Požadované zboží již není na skladě!", "Varování",
+                        JOptionPane.WARNING_MESSAGE);
             }
             lbCena.setText(kosik.cenaCelkem());
         });
-        btOdeberJeden.addActionListener((e)-> {
+        btOdeberJeden.addActionListener((e) -> {
             int radek = tabulkaKosiku.getSelectedRow();
             Zbozi zbozi = (Zbozi) tabulkaKosiku.getValueAt(radek, 0);
             kosik.snizit(radek);
             sklad.pridejJednoZbozi(zbozi);
-            if((int) tabulkaKosiku.getValueAt(radek, 2) <= 0) {
+            if ((int) tabulkaKosiku.getValueAt(radek, 2) <= 0) {
                 kosik.odeber(radek);
             }
             lbCena.setText(kosik.cenaCelkem());
         });
-        btSmaz.addActionListener((e)-> {
+        btSmaz.addActionListener((e) -> {
             int radek = tabulkaKosiku.getSelectedRow();
             Zbozi zbozi = (Zbozi) tabulkaKosiku.getValueAt(radek, 0);
             int pocet = (int) tabulkaKosiku.getValueAt(radek, 2);
@@ -256,7 +294,7 @@ public class Obchod {
             kosik.odeber(radek);
             lbCena.setText(kosik.cenaCelkem());
         });
-        btKup.addActionListener((e)-> {
+        btKup.addActionListener((e) -> {
             JOptionPane.showMessageDialog(hlavniPanel, "bude brzy", "xdd", JOptionPane.INFORMATION_MESSAGE);
         });
 
@@ -268,7 +306,6 @@ public class Obchod {
         pnTlacitka.add(btSmaz);
         pnTlacitka.add(btKup);
 
-
         panelKosiku = new JPanel();
         panelKosiku.setLayout(new BorderLayout());
         panelKosiku.add(spTabulkaKosiku, BorderLayout.CENTER);
@@ -277,17 +314,51 @@ public class Obchod {
 
     public static void vytvorHlavniOkno() {
         IntelliJTheme.setup(Obchod.class.getResourceAsStream("/lib/flatlaf/mocha.theme.json"));
+        Obchod obchod = new Obchod();
         JFrame hlavniOkno = new JFrame();
-        hlavniOkno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        hlavniOkno.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        hlavniOkno.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                boolean saved = false;
+                int confirmed = JOptionPane.showConfirmDialog(null,
+                        "Chcete uložit změny?", "Pozor",
+                        JOptionPane.YES_NO_CANCEL_OPTION);
+                if (confirmed == JOptionPane.CANCEL_OPTION) {
+                    hlavniOkno.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+                } else if (confirmed == JOptionPane.YES_OPTION) {
+                    try {
+                        JFileChooser dialog = new JFileChooser(".");
+                        dialog.setFileFilter(new FileNameExtensionFilter("*.json", "json"));
+                        if (dialog.showSaveDialog(hlavniOkno) == JFileChooser.APPROVE_OPTION) {
+                            String soubor = dialog.getSelectedFile().getPath();
+                            obchod.getSklad().uloz(new GsonSerDes(), soubor + ".json");
+                            saved = true;
+                        }
+                    } catch (Exception exp) {
+                        JOptionPane.showMessageDialog(hlavniOkno,
+                                "Při ukládání do JSON formátu nastala: "
+                                        + exp.getLocalizedMessage(),
+                                "Chyba ukládání",
+                                JOptionPane.ERROR_MESSAGE);
+                    }
+                    if (saved) {
+                        hlavniOkno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    }
+                } else if (confirmed == JOptionPane.NO_OPTION) {
+                    hlavniOkno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                }
+            }
+        });
+        // hlavniOkno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         hlavniOkno.setTitle("Obchod");
         hlavniOkno.setLocationByPlatform(true);
 
-        Obchod obchod = new Obchod();
         hlavniOkno.setContentPane(obchod.getHlavniPanel());
         hlavniOkno.setJMenuBar(obchod.getNabidka());
         hlavniOkno.pack();
         hlavniOkno.setVisible(true);
     }
+
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> vytvorHlavniOkno());
     }
